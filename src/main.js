5f7305d7cca48e85192d9bbdb7276765191c3b8b
a1002427060 <1002427060@qq.com>
2019-02-12 20:53:49 +0800

    完成home的页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import './lib/mui/css/mui.min.css'
+import '@/style/common.css'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+
 Vue.use(Mint)
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index.vue'
-
+import HomeContainer from "@/pages/HomeContainer"
+import MemberContainer from "@/pages/MemberContainer"
+import ShopcarContainer from "@/pages/ShopcarContainer"
+import SearchContainer from "@/pages/SearchContainer"
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      component: index
-    }
-  ]
+   {path: '/',redirect:"/home"},
+   {path:'/home',component:HomeContainer},
+   {path:'/member',component:MemberContainer},
+   {path:'/shopcar',component:ShopcarContainer},
+   {path:'/search',component:SearchContainer},
+  ],
+  linkActiveClass:"mui-active"
 })
